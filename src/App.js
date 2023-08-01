aa2adc30dde5ca56201dac073c505e42c1a55b22
ozge <ozge.belenn@gmail.com>
2023-08-01 12:07:36 +0300

    map fonksiyonu ile item oluşturdu. Boolean ile line-through style verildi

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
+];
+
 function App() {
   return (
     <div className="app">
@@ -22,8 +28,28 @@ const Form = () => {
   );
 };
 const PackingList = () => {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 };
+
+const Item = ({ item }) => {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+};
+
 const Stats = () => {
   return (
     <footer>
