48d21c7ca9614236b09e1528e7a7ef4d00cbe5be
ozge <ozge.belenn@gmail.com>
2023-08-01 23:53:54 +0300

    value değerleri alındı yeni bir newItem objesi yaratıldı

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: true },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -21,16 +23,27 @@ const Logo = () => {
   return <h1> 🏝 Far Away 🧳 </h1>;
 };
 const Form = () => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  const newItem = {
+    description,
+    quantity,
+    id: "id" + Math.random().toString(16).slice(2),
+    package: false,
+  };
   // tarayıcının tekrar kendini güncellemesini engellerler.
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e);
   };
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip? </h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {/* 1'den 20ye kadar otomatik hesaplama fonksiyonu */}
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
@@ -38,7 +51,13 @@ const Form = () => {
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      {/* bir veriyi almak 3 adımda gerçekleşir. 1. state yarat 2. value ile değeri al  3. set fonksiyonu ile değişen değeri al */}
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
@@ -55,11 +74,12 @@ const PackingList = () => {
   );
 };
 
-const Item = ({ item }) => {
+const Item = ({ item, newItem }) => {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
+        {newItem}
       </span>
       <button>❌</button>
     </li>
